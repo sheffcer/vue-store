4d916f4feecd945b59911cbad96b1b34ca58fd95
Alex sheffcer <a.sheffcer@gmail.com>
2021-02-01 16:25:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <div class="card">
+      <h1>Title</h1>
+      <h2>Счетчик {{$store.state.counter}}</h2>
+      <button class="btn primary" @click="$store.state.counter++">Добавить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from './TheNavbar'
+
+export default {
+  components: { TheNavbar }
+}
+</script>
---
+++ src/TheNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="navbar">
+    <strong>Счетчик {{ counter }}</strong>
+
+    <button class="btn" @click="add">Добавить 5</button>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
